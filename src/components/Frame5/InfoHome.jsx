fa81c73d7f19e36caa506b76b62cd01021beb334
Vikas24Chauhan <vikas24chauhan@gmail.com>
2024-11-21 15:28:01 +0530

    done some changes

--- src/components/Frame5/InfoHome.jsx
+++ src/components/Frame5/InfoHome.jsx
@@ -5,7 +5,6 @@ import Logo from "/images/Janus_PR_Log 1.png";
 function InfoHome() {
   return (
     <div className="infoHome-main-div">
-      <img className="infoHome-logo" src={Logo} alt="Project Logo" />
       <h2>Corporate Communication</h2>
       <p>
         Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
