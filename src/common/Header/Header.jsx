fb2ddba5d9e930841b0a3abb587b59621bf1f307
Vikas24Chauhan <vikas24chauhan@gmail.com>
2024-11-21 13:29:55 +0530

    navbar updated

--- src/common/Header/Header.jsx
+++ src/common/Header/Header.jsx
@@ -1,11 +1,25 @@
-import React from "react";
-import { NavLink } from "react-router-dom";
+import React, { useEffect, useState } from "react";
 import "./Header.css";
+import { NavLink } from "react-router-dom";
+import menuIcon from "/public/images/menu-icon.png";
 
 function Header() {
+  const [sticky, setSticky] = useState(false);
+
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      window.scrollY > 50 ? setSticky(true) : setSticky(false);
+    });
+  }, []);
+
+  const [mobileMenu, setMobileMenu] = useState(false);
+  const toggleMenu = () => {
+    mobileMenu ? setMobileMenu(false) : setMobileMenu(true);
+  };
+
   return (
-    <div className="header-container">
-      <ul>
+    <nav className={`container ${sticky ? "dark-nav" : ""}`}>
+      <ul className={mobileMenu ? "" : "hide-mobile-menu"}>
         <li>
           <NavLink to="/">Home</NavLink>
         </li>
@@ -22,7 +36,13 @@ function Header() {
           <NavLink to="/more-information">More-Information</NavLink>
         </li>
       </ul>
-    </div>
+      <img
+        src={menuIcon}
+        alt="Menu Icon"
+        className="menu-icon"
+        onClick={toggleMenu}
+      />
+    </nav>
   );
 }
 
