56a4531a7ca820634cd33284ddeee5f8b74c705e
Vikas24Chauhan <vikas24chauhan@gmail.com>
2024-11-21 14:13:58 +0530

    Frame3 code updated

--- src/components/Frame3/ContactUs1.jsx
+++ src/components/Frame3/ContactUs1.jsx
@@ -1,17 +1,10 @@
 import React from "react";
 import "./ContactUs1.css";
-import Logo from "/images/Janus_PR_Log 1.png";
-import Phone from "/images/Phone.png";
-import Whatsapp from "/images/Whatsapp.png";
 
 function ContactUs1() {
   return (
     <div className="contact1-main-div">
-      <img className="contact1-logo" src={Logo} alt="Project Logo" />
       <h2>Contact Us</h2>
-      <img className="contact1-phone" src={Phone} alt="Phone Logo" />
-      <br />
-      <img className="contact1-whatsapp" src={Whatsapp} alt="WhatsApp Logo" />
     </div>
   );
 }
