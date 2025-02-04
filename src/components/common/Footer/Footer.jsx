51b9ba1497feeccbd2d058ea047b9c15175c05cd
Vikas24Chauhan <vikas24chauhan@gmail.com>
2025-02-04 17:41:42 +0530

    navbar and footer are updated

--- src/components/common/Footer/Footer.jsx
+++ src/components/common/Footer/Footer.jsx
@@ -1,66 +1,35 @@
 import React from "react";
 import "./Footer.css";
-import Facebook from "/images/Facebook.png";
-import Instagram from "/images/Instagram.png";
-import X from "/images/X.png";
-import Youtube from "/images/Youtube.png";
+import LinkedIn from "/public/images/linkedin.png";
+import Mail from "/public/images/mail.webp";
 
 function Footer() {
+  const handleLinkedIn = () => {
+    window.open("https://www.linkedin.com/", "_blank");
+  };
+
+  const handleMail = () => {
+    const recipient = "info@entrepreneurshipnetwork.net";
+    const gmailUrl = `https://mail.google.com/mail/?view=cm&fs=1&to=${recipient}`;
+    window.open(gmailUrl, "_blank");
+  };
+
   return (
-    <div className="footer-main-div">
-      <div className="upper-div">
-        <h1>CONNECT</h1>
-        <h2>GET IN TOUCH</h2>
-        <hr />
-      </div>
-      <div className="lower-div">
-        <div className="quick-links">
-          <h4>Quick Links</h4>
-          <ul>
-            <li>Home</li>
-            <li>About Us</li>
-            <li>Testimonial</li>
-            <li>PR Solution</li>
-            <li>Agency</li>
-          </ul>
+    <div className="footer">
+      <hr />
+      <div className="footer-div">
+        <div className="footer-social">
+          <img onClick={handleLinkedIn} src={LinkedIn} alt="LinkedIn logo" />
+          <img
+            style={{ marginLeft: "1rem" }}
+            onClick={handleMail}
+            src={Mail}
+            alt="mail logo"
+          />
         </div>
-        <div className="head-office">
-          <h4>Head Office</h4>
-          <h5>
-            66, Second Floor, Atul Grove Road, Janpath, Connaught Place, New
-            Delhi, Delhi 110001
-          </h5>
+        <div className="footer-contant">
+          <h6>© 2024 All Right Reserved</h6>
         </div>
-        <div className="contact-info">
-          <h2>Call Us</h2>
-          <h5>+91 9876543210</h5>
-          <h2>Mail Us</h2>
-          <h5>abc123@gmail.com</h5>
-        </div>
-        <div className="follow-us">
-          <h4>Follow Us</h4>
-          <div className="social-info-div">
-            <div className="logo-div">
-              <img src={Facebook} alt="Facebook Logo" />
-              <br />
-              <img src={Instagram} alt="Instagram Logo" />
-              <br />
-              <img src={X} alt="X Logo" />
-              <br />
-              <img src={Youtube} alt="Youtube Logo" />
-            </div>
-            <div className="text-div">
-              <h5>Facebook</h5>
-              <h5>Instagram</h5>
-              <h5>Twitter</h5>
-              <h5>Youtube</h5>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div className="last-div">
-        <hr />
-        <h6>Copyright Janus PR India 2024 All Right Reserved</h6>
       </div>
     </div>
   );
--- src/components/common/Header/Header.jsx
+++ src/components/common/Header/Header.jsx
@@ -1,48 +1,49 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import "./Header.css";
 import { NavLink } from "react-router-dom";
-import menuIcon from "/images/menu-icon.png";
+import Logo from "/public/images/logo.png";
+import { RxHamburgerMenu } from "react-icons/rx";
 
 function Header() {
-  const [sticky, setSticky] = useState(false);
+  const [showNav, setShowNav] = useState(false);
 
-  useEffect(() => {
-    window.addEventListener("scroll", () => {
-      window.scrollY > 50 ? setSticky(true) : setSticky(false);
-    });
-  }, []);
+  const handleButtonToggle = () => {
+    setShowNav(!showNav);
+  };
 
-  const [mobileMenu, setMobileMenu] = useState(false);
-  const toggleMenu = () => {
-    mobileMenu ? setMobileMenu(false) : setMobileMenu(true);
+  const handleCloseNav = () => {
+    setShowNav(false);
   };
 
   return (
-    <nav className={`container ${sticky ? "dark-nav" : ""}`}>
-      <ul className={mobileMenu ? "" : "hide-mobile-menu"}>
-        <li>
-          <NavLink to="/">Home</NavLink>
-        </li>
-        <li>
-          <NavLink to="/about">AboutUs</NavLink>
-        </li>
-        <li>
-          <NavLink to="/contact">ContactUs</NavLink>
-        </li>
-        <li>
-          <NavLink to="/services">Services</NavLink>
-        </li>
-        <li>
-          <NavLink to="/more-information">More-Information</NavLink>
-        </li>
-      </ul>
-      <img
-        src={menuIcon}
-        alt="Menu Icon"
-        className="menu-icon"
-        onClick={toggleMenu}
-      />
-    </nav>
+    <header className="header">
+      <div className="logo">
+        <img src={Logo} alt="website logo" />
+      </div>
+      <nav className={showNav ? "mobile-nav" : "web-nav"}>
+        <NavLink to="/" onClick={handleCloseNav}>
+          Home
+        </NavLink>
+        <NavLink to="/about" onClick={handleCloseNav}>
+          AboutUs
+        </NavLink>
+        <NavLink to="/contact" onClick={handleCloseNav}>
+          ContactUs
+        </NavLink>
+        <NavLink to="/services" onClick={handleCloseNav}>
+          Services
+        </NavLink>
+        <NavLink to="/more-information" onClick={handleCloseNav}>
+          More-Information
+        </NavLink>
+      </nav>
+
+      <div className="hamburger">
+        <button onClick={handleButtonToggle}>
+          <RxHamburgerMenu />
+        </button>
+      </div>
+    </header>
   );
 }
 
